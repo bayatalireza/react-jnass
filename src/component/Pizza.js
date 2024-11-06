0791b7d1c92010f6d99c087c4b9ce18877f75bbe
alireza <bayatalireza86@gmail.com>
2024-11-06 21:04:46 +0330

    Add the required files of the new repository

---
+++ src/component/Pizza.js
@@ -0,0 +1,10 @@
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/focaccia.jpg" alt="focaccia" />
+      <h2>Focaccia</h2>;<p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
+
+export default Pizza;
